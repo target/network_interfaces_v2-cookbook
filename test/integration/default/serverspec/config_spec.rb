377dc095c9466845e8a111a2f71c9cdd1e02ced3
Jacob McCann <jacob.mccann2@target.com>
2015-04-01 16:06:46 -0500

    Add checking config file contents to integration testing

--- test/integration/default/serverspec/config_spec.rb
+++ test/integration/default/serverspec/config_spec.rb
@@ -3,28 +3,34 @@ require 'spec_helper'
 describe 'creates network configuration files' do
   if rhel?
     context 'rhel family' do
-      it 'creates config for eth0' do
-        expect(file('/etc/sysconfig/network-scripts/ifcfg-eth0')).to be_file
+      describe file('/etc/sysconfig/network-scripts/ifcfg-eth0') do
+        it { should be_file }
+        it { should contain File.read File.join(File.dirname(__FILE__), 'fixtures', 'ifcfg-eth0') }
       end
-      it 'creates config for eth1' do
-        expect(file('/etc/sysconfig/network-scripts/ifcfg-eth1')).to be_file
+      describe file('/etc/sysconfig/network-scripts/ifcfg-eth1') do
+        it { should be_file }
+        it { should contain File.read File.join(File.dirname(__FILE__), 'fixtures', 'ifcfg-eth1') }
       end
-      it 'creates config for eth2' do
-        expect(file('/etc/sysconfig/network-scripts/ifcfg-eth2')).to be_file
+      describe file('/etc/sysconfig/network-scripts/ifcfg-eth2') do
+        it { should be_file }
+        it { should contain File.read File.join(File.dirname(__FILE__), 'fixtures', 'ifcfg-eth2') }
       end
     end
   end
 
   if debian?
     context 'debian family' do
-      it 'creates config for eth0' do
-        expect(file('/etc/network/interfaces.d/eth0')).to be_file
+      describe file('/etc/network/interfaces.d/eth0') do
+        it { should be_file }
+        it { should contain File.read File.join(File.dirname(__FILE__), 'fixtures', 'eth0') }
       end
-      it 'creates config for eth1' do
-        expect(file('/etc/network/interfaces.d/eth1')).to be_file
+      describe file('/etc/network/interfaces.d/eth1') do
+        it { should be_file }
+        it { should contain File.read File.join(File.dirname(__FILE__), 'fixtures', 'eth1') }
       end
-      it 'creates config for eth2' do
-        expect(file('/etc/network/interfaces.d/eth2')).to be_file
+      describe file('/etc/network/interfaces.d/eth2') do
+        it { should be_file }
+        it { should contain File.read File.join(File.dirname(__FILE__), 'fixtures', 'eth2') }
       end
     end
   end
