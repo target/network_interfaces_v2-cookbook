2d8ecb49536bddfea27addd7cb2fd8924d3ef8a7
Jacob McCann <jacob.mccann2@target.com>
2015-04-06 21:03:09 -0500

    Apache 2.0 license stuff

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name             'network_interfaces_v2'
 maintainer       'Jacob McCann'
 maintainer_email 'jacob.mccann2@target.com'
-license          'All rights reserved'
-description      'Installs/Configures network_interfaces_v2'
-long_description 'Installs/Configures network_interfaces_v2'
+license          'Apache 2.0'
+description      'A cookbook containing providers for defining network interfaces'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends 'modules', '~> 0.2.0'
