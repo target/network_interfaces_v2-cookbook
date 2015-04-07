2728449d4354d9804cb96e44384d853a2a116f42
Jacob McCann <jacob.mccann2@target.com>
2015-04-06 20:10:54 -0500

    Remove logic for installing ruby-wmi gem
    Handled by busser-serverspec:
    https://github.com/test-kitchen/busser-serverspec/pull/25

--- test/integration/vlan/serverspec/spec_helper.rb
+++ test/integration/vlan/serverspec/spec_helper.rb
@@ -24,23 +24,6 @@ def rhel?
   File.exist? '/etc/redhat-release'
 end
 
-def install_wmi
-  puts 'Installing wmi-lite gem'
-  `#{Gem.bindir}/bundle install --gemfile #{File.join(File.dirname(__FILE__), 'Gemfile')}`
-  Gem.clear_paths
-end
-
-def load_wmi
-  puts 'Loading wmi-lite gem'
-  begin
-    gem 'wmi-lite'
-  rescue LoadError
-    install_wmi
-  end
-
-  require 'wmi-lite'
-end
-
 def win_interface(name)
   net_adapters.find { |n| n['netconnectionid'] == name }
 end
@@ -53,6 +36,6 @@ end
 set :backend, :exec unless windows?
 
 # Windows
-load_wmi if windows?
+require 'wmi-lite' if windows?
 set :backend, :cmd if windows?
 set :os, :family => 'windows' if windows?
