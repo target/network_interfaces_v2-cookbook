2e8c71d5d42a84e779013282dedf522d63896bec
Jacob McCann <jacob.mccann2@target.com>
2015-04-09 08:12:04 -0500

    Version 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'jacob.mccann2@target.com'
 license          'Apache 2.0'
 description      'A cookbook containing providers for defining network interfaces'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
 
 depends 'modules', '~> 0.2.0'
