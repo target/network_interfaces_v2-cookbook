306dfd5b38c93a43e7242c8824a61e18002677ae
Jacob McCann <jacob.mccann2@target.com>
2015-04-28 15:54:33 -0500

    Add testing to show correct platform specific provider is used

--- test/fixtures/cookbooks/fake/recipes/bonding.rb
+++ test/fixtures/cookbooks/fake/recipes/bonding.rb
@@ -3,21 +3,21 @@ network_interface 'eth0' unless node['os'] == 'windows'
 
 case node['platform_family']
 when 'rhel', 'fedora'
-  rhel_network_interface 'eth1' do
+  network_interface 'eth1' do
     bootproto 'none'
     bond_master 'bond0'
   end
 
-  rhel_network_interface 'eth2' do
+  network_interface 'eth2' do
     bootproto 'none'
     bond_master 'bond0'
   end
 
-  rhel_network_interface 'bond0' do
+  network_interface 'bond0' do
     bond_mode 'mode=1 miimon=100'
   end
 when 'debian'
-  debian_network_interface 'bond0' do
+  network_interface 'bond0' do
     bootproto 'static'
     address '12.12.12.10'
     netmask '255.255.255.0'
