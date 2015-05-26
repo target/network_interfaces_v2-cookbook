f73ee5d693fd1e0876e77923aaa4bb0bda025348
Jacob McCann <jacob.mccann2@target.com>
2015-05-26 13:21:00 -0500

    Add list of supported platforms

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,9 @@ description      'Providers for configuring network on Ubuntu, RHEL, and Windows
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.1.1'
 
+supports 'ubuntu', '>= 12.04'
+supports 'windows'
+supports 'redhat', '~> 6.0'
+supports 'centos', '~> 6.0'
+
 depends 'modules', '~> 0.2.0'
