a9fbd7305607f6694a2120d02a9d8eed90577bad
Felix <felix@cds.eb.mil.br>
2015-09-08 16:33:48 -0300

    Add dns_nameservers for Debian.

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             'network_interfaces_v2'
-maintainer       'Jacob McCann'
-maintainer_email 'jacob.mccann2@target.com'
+maintainer       'C2 Team'
+maintainer_email 'pacificador@defesa.mil.br'
 license          'Apache 2.0'
 description      'Providers for configuring network on Ubuntu, RHEL, and Windows'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.3.0'
+version          '2.4.0'
 
 supports 'ubuntu', '>= 12.04'
 supports 'windows'
