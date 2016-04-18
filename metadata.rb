5f74e99bc62e04911eef637518f60b362866c015
Jacob McCann <jacob.mccann2@target.com>
2016-04-18 09:29:11 -0500

    Address foodcritic warnings

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,8 @@ maintainer_email 'jacob.mccann2@target.com'
 license          'Apache 2.0'
 description      'Providers for configuring network on Ubuntu, RHEL, and Windows'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+source_url       'https://github.com/target/network_interfaces_v2-cookbook'
+issues_url       'https://github.com/target/network_interfaces_v2-cookbook/issues'
 version          '2.7.0'
 
 supports 'ubuntu', '>= 12.04'
