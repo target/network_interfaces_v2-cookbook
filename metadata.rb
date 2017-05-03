4a99abf79776a347c1a08c77be55c8d0f66d4c9d
Robin H. Johnson <robbat2@gentoo.org>
2017-05-03 12:04:28 -0700

    metadata: fix license string.
    Signed-off-by: Robin H. Johnson <robbat2@gentoo.org>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'network_interfaces_v2'
 maintainer       'Jacob McCann'
 maintainer_email 'jacob.mccann2@target.com'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Providers for configuring network on Ubuntu, RHEL, and Windows'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url       'https://github.com/target/network_interfaces_v2-cookbook'
